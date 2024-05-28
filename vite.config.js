dbd8ddda215cb38e5a6ef7e17fb7c787844686d5
SIMPLE-TECNIQUES <shankarcode2000@gmail.com>
2024-05-28 13:24:24 +0530

    change in vite configration

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		outDir: 'dist', // This is the default value
+	  },
 	server: {
 		port: 3000,
 		proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				target: "https://chatappfinal-fvth.onrender.com",
 			},
 		},
 	},
